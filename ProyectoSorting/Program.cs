a0f224c4484c9de4cf2c8ae734e3e747ea54815a
DanielLopez09 <92112665+DanielLopez09@users.noreply.github.com>
2022-05-01 21:47:35 +0200

    Agregar archivos de proyecto.

---
+++ ProyectoSorting/Program.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ProyectoSorting
+{
+    public class SortingArray
+    {
+        public int[] array;
+        public int[] arrayCreciente;
+        public int[] arrayDecreciente;
+
+        public SortingArray(int elements, Random random)
+        {
+            array = new int[elements];
+            arrayCreciente = new int[elements];
+            arrayDecreciente = new int[elements];
+            for (int i = 0; i < elements; i++)
+            {
+                array[i] = random.Next(100);
+            }
+            Array.Copy(array, arrayCreciente, elements);
+            Array.Sort(arrayCreciente);
+            Array.Copy(arrayCreciente, arrayDecreciente, elements);
+            Array.Reverse(arrayDecreciente);
+        }
+
+        public void Sort(Action<int[]> func)
+        {
+            Stopwatch time = new Stopwatch();
+            int[] temp = new int[array.Length];
+            Array.Copy(array, temp, array.Length);
+
+            Console.WriteLine(func.Method.Name);
+
+            time.Start();
+
+            func(temp);
+
+            time.Stop();
+
+            Console.WriteLine("Initial: " + time.ElapsedMilliseconds + "ms " + time.ElapsedTicks + "ticks");
+
+            time.Reset();
+
+            time.Start();
+
+            func(temp);
+
+            time.Stop();
+
+            Console.WriteLine("Increasing: " + time.ElapsedMilliseconds + "ms " + time.ElapsedTicks + "ticks");
+
+            time.Reset();
+
+            Array.Reverse(temp);
+
+            time.Start();
+
+            func(temp);
+
+            time.Stop();
+
+            Console.WriteLine("Decreasing: " + time.ElapsedMilliseconds + "ms " + time.ElapsedTicks + "ticks");
+        }
+
+        //BubbleSort
+        public void BubbleSort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+        public void BubbleSortEarlyExit(int[] array)
+        {
+            bool ordered = true;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                ordered = true;
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        ordered = false;
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+                if (ordered)
+                    return;
+            }
+        }
+
+        //QuickSort
+        public void QuickSort(int[] array)
+        {
+            QuickSort(array, 0, array.Length - 1);
+        }
+        public void QuickSort(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int pivot = QuickSortPivot(array, left, right);
+                QuickSort(array, left, pivot);
+                QuickSort(array, pivot + 1, right);
+            }
+        }
+        public int QuickSortPivot(int[] array, int left, int right)
+        {
+            int pivot = array[(left + right) / 2];
+            while (true)
+            {
+                while (array[left] < pivot)
+                {
+                    left++;
+                }
+                while (array[right] > pivot)
+                {
+                    right--;
+                }
+                if (left >= right)
+                {
+                    return right;
+                }
+                else
+                {
+                    int temp = array[left];
+                    array[left] = array[right];
+                    array[right] = temp;
+                    right--; left++;
+                }
+            }
+        }
+
+        //MergeSort
+        public void MergeSort(int[] array)
+        {
+            MergeSort(array, 0, array.Length - 1, 0);
+        }
+        public void MergeSort(int[] array, int left, int right, int medium)
+        {
+            int n1 = medium - left + 1;
+            int n2 = right - medium;
+
+            int[] Left = new int[n1];
+            int[] Right = new int[n2];
+            int i, j;
+
+            for (i = 0; i < n1; ++i)
+            {
+                Left[i] = array[left + i];
+
+            }
+            for (j = 0; j < n2; ++j)
+            {
+                Right[j] = array[medium + 1 + j];
+
+            }
+
+            i = 0;
+            j = 0;
+
+            int k = left;
+            while (i < n1 && j < n2)
+            {
+                if (Left[i] <= Right[j])
+                {
+                    array[k] = Left[i];
+                    i++;
+                }
+                else
+                {
+                    array[k] = Right[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while (i < n1)
+            {
+                array[k] = Left[i];
+                i++;
+                k++;
+            }
+            while (j < n2)
+            {
+                array[k] = Right[j];
+                j++;
+                k++;
+            }
+        }
+        void mergesort(int[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int medium = left + (right - left) / 2;
+
+                mergesort(array, left, medium);
+                mergesort(array, medium + 1, right);
+
+                MergeSort(array, left, medium, right);
+            }
+        }
+
+        //HeapSort
+        public void HeapSort(int[] array)
+        {
+            int n = array.Length;
+
+            for (int i = n / 2 - 1; i >= 0; i--)
+            {
+                heap(array, n, i);
+            }
+
+            for (int i = n - 1; i > 0; i--)
+            {
+                int temp = array[0];
+                array[0] = array[i];
+                array[i] = temp;
+
+                heap(array, i, 0);
+            }
+        }
+        void heap(int[] array, int n, int i)
+        {
+            int largest = i;
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+
+            if(left < n && array[left] > array[largest])
+            {
+                largest = left;
+            }
+
+            if (right < n && array[right] > array[largest])
+            {
+                largest = right;
+            }
+
+            if(largest != i)
+            {
+                int swap = array[i];
+                array[i] = array[largest];
+                array[largest] = swap;
+
+                heap(array, n, largest);
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many elements do you want?");
+            int elements = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What seed do you want to use?");
+            int seed = int.Parse(Console.ReadLine());
+
+            Random random = new Random(seed);
+            SortingArray array = new SortingArray(elements, random);
+            array.Sort(array.BubbleSort);
+            array.Sort(array.BubbleSortEarlyExit);
+            array.Sort(array.QuickSort);
+            array.Sort(array.MergeSort);
+            array.Sort(array.HeapSort);
+        }
+    }
+}
+
